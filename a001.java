8db72b477f85bc8cbde70fbae441a2ad62e7e9ca
Anemois <46663053+Anemois@users.noreply.github.com>
2023-08-25 17:23:09 +0800

    Add files via upload
    add a001

---
+++ a001.java
@@ -0,0 +1,13 @@
+package com.anemois;
+
+import java.util.Scanner;
+
+public class a001{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        scanner.close();
+        System.out.print("hello, ");
+        System.out.println(str);
+    }
+}
